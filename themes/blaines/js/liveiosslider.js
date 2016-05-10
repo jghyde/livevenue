0dd7b134ca762ba097abc0c96aa57d05ef77d94e
Joe Hyde <joeh@hydeinteractive.com>
2016-05-10 10:42:33 -0500

    theme updates

--- themes/blaines/js/liveiosslider.js
+++ themes/blaines/js/liveiosslider.js
@@ -2,47 +2,31 @@
 
     Drupal.behaviors.blaines = {
         attach: function (context, settings) {
-
-/* Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing. https://davidwalsh.name/javascript-debounce-function */
-            function debounce(func, wait, immediate) {
-                var timeout;
-                return function() {
-                    var context = this, args = arguments;
-                    var later = function() {
-                        timeout = null;
-                        if (!immediate) func.apply(context, args);
-                    };
-                    var callNow = immediate && !timeout;
-                    clearTimeout(timeout);
-                    timeout = setTimeout(later, wait);
-                    if (callNow) func.apply(context, args);
-                };
+            var w = $(window).width();
+            var h = $(window).height();
+            var useheight = h;
+            var properheight = w * (9/16);
+            if (h < properheight) {
+                useheight = h;
             }
-            var myEfficientFn = debounce(function() {
-                var w = $(window).width();
-                var h = $(window).height();
-                var useheight = h;
-                var properheight = w * (9/16);
-                if (h < properheight) {
-                    useheight = h;
-                }
-                else {
-                    useheight = properheight;
-                }
-                $('.iosslider').width(w).height(useheight);
-                $('.slider').width(w).height(useheight);
-                $('.slide').width(w).height(useheight).css('background-size', 'cover');
-                //$('.iosslider').height(w * (9/16));
-            }, 250);
-            window.addEventListener('resize', myEfficientFn);
+            else {
+                useheight = properheight;
+            }
+            $('#block-views-block-ios-slider-iosslider').height(useheight);
+            $('.iosslider').height(useheight);
+            $('.slider').height(useheight);
+            //$('.slide').height(useheight);
             $('.iosslider').iosSlider({
                 snapToChildren: true,
-                scrollbar: true,
-                scrollbarHide: true,
+                keyboardControls: true,
+                responsiveSlideContainer: true,
+                responsiveSlides: true,
                 desktopClickDrag: true,
+                infiniteSlider: true,
+                autoSlide: true,
+                sliderCSS: {
+                    height: useheight,
+                },
             });
         }
     };
